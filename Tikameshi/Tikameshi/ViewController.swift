28821e21de4f2396d837c4c43103586374bf6fe4
shibata-takezou <takezou0317@gmail.com>
2024-02-09 23:53:58 +0900

    プロジェクト作成と設定
    アプリ「チカメシ」のプロジェクトの作成と設定を行った

---
+++ Tikameshi/Tikameshi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tikameshi
+//
+//  Created by 柴田武蔵 on 2024/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

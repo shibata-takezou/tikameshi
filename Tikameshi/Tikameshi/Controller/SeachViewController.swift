b7aab59eeca42da7f93dfe4b327f8d5be2502d7e
shibata-takezou <takezou0317@gmail.com>
2024-02-11 23:40:24 +0900

    mainVCの名前変更

--- Tikameshi/Tikameshi/Controller/SeachViewController.swift
+++ Tikameshi/Tikameshi/Controller/SeachViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SeachViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white

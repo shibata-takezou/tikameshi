40b351882aff618b15b4ccec1469e1de725ffe28
shibata-takezou <takezou0317@gmail.com>
2024-02-13 19:13:43 +0900

    SearchVCのview表示

--- Tikameshi/Tikameshi/Controller/SearchViewController.swift
+++ Tikameshi/Tikameshi/Controller/SearchViewController.swift
@@ -8,24 +8,17 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-    var distanceSC: DistanceSegmentedControl!
-    let distance = ["300m","500m","1000m","2000m","3000m"]
-    var searchButton: SearchButton!
+    var searchVCStackView: SearchVCStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "検索"
-        self.distanceSC = DistanceSegmentedControl(items: self.distance)
-        self.view.addSubview(self.distanceSC)
-        self.distanceSC.translatesAutoresizingMaskIntoConstraints = false
-        self.searchButton = SearchButton()
-        self.view.addSubview(self.searchButton)
-        self.searchButton.translatesAutoresizingMaskIntoConstraints = false
+        self.searchVCStackView = SearchVCStackView(frame: CGRect())
+        self.view.addSubview(self.searchVCStackView)
+        self.searchVCStackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            self.distanceSC.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0),
-            self.distanceSC.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0),
-            self.distanceSC.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.95),
-            self.searchButton.topAnchor.constraint(equalTo: self.distanceSC.bottomAnchor, constant: 20),
-            self.searchButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0)
+            self.searchVCStackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0),
+            self.searchVCStackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0),
+            self.searchVCStackView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1.0)
         ])
     }
 }

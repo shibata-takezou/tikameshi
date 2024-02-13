8d6ab23258b5a88fd4557fb9df8cfa341cde88c3
shibata-takezou <takezou0317@gmail.com>
2024-02-13 19:25:04 +0900

    SearchVCのリファクタ（強制アンラップの削除）

--- Tikameshi/Tikameshi/Controller/SearchViewController.swift
+++ Tikameshi/Tikameshi/Controller/SearchViewController.swift
@@ -8,17 +8,15 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-    var searchVCStackView: SearchVCStackView!
+    var searchVCStackView = SearchVCStackView(frame: CGRect())
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "検索"
-        self.searchVCStackView = SearchVCStackView(frame: CGRect())
         self.view.addSubview(self.searchVCStackView)
         self.searchVCStackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             self.searchVCStackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0),
             self.searchVCStackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0),
-            self.searchVCStackView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1.0)
         ])
     }
 }

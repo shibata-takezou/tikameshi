dce0e04fa805a452d7c8757f9b9096ac65f2e851
shibata-takezou <takezou0317@gmail.com>
2024-02-19 14:46:46 +0900

    detailViewに配置するStackViewの作成

--- Tikameshi/Tikameshi/Controller/DetailViewController.swift
+++ Tikameshi/Tikameshi/Controller/DetailViewController.swift
@@ -9,12 +9,27 @@ import UIKit
 
 class DetailViewController: UIViewController {
     var restaurant: Restaurant!
+    var detailVCStackView = UIStackView()
+    var mapAppButton = ScreenTransitionButton(text: "案内をスタートする")
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "詳細"
         self.view.backgroundColor = .vcColor
-        print("\(restaurant.name)")
-        print("\(restaurant.open)")
-        print("\(restaurant.address)")
+        self.detailVCStackView = DetailVCView(name: restaurant.name, address: restaurant.address, open: restaurant.open, topImageView: LogoImageView(logoImageURL: restaurant.logo_image))
+        self.view.addSubview(self.detailVCStackView)
+        self.detailVCStackView.translatesAutoresizingMaskIntoConstraints = false
+        self.mapAppButton.addTarget(self, action: #selector(self.transitionApp(_:)), for: .touchUpInside)
+        self.view.addSubview(self.mapAppButton)
+        self.mapAppButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.detailVCStackView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            self.detailVCStackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.detailVCStackView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8),
+            self.mapAppButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.mapAppButton.topAnchor.constraint(equalTo: self.detailVCStackView.bottomAnchor, constant: 20)
+        ])
+    }
+    @objc func transitionApp(_ sender: UIButton) {
+        print("mapアプリへ移動します。")
     }
 }
---
+++ Tikameshi/Tikameshi/View/DetailVC/DetailVCStackView.swift
@@ -0,0 +1,65 @@
+//
+//  DetailVCView.swift
+//  Tikameshi
+//
+//  Created by 柴田武蔵 on 2024/02/19.
+//
+
+import UIKit
+
+class DetailVCView: UIStackView {
+    var name: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 24)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    var address: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    var open: UILabel =  {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.backgroundColor = .white
+        label.layer.cornerRadius = 10
+        label.clipsToBounds = true
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    var topImageView = UIImageView()
+    init(name: String, address: String, open: String, topImageView: LogoImageView) {
+        super.init(frame: CGRect())
+        self.name.text = name
+        self.address.text = address
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 8
+        let openText = NSAttributedString(string: open, attributes: [NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        self.open.attributedText = openText
+        self.topImageView = topImageView
+        self.setUP()
+    }
+    private func setUP() {
+        self.axis = .vertical
+        self.alignment = .leading
+        self.spacing = 20
+        self.addArrangedSubview(self.name)
+        self.addArrangedSubview(self.topImageView)
+        self.topImageView.translatesAutoresizingMaskIntoConstraints = false
+        self.addArrangedSubview(self.address)
+        self.addArrangedSubview(self.open)
+        NSLayoutConstraint.activate([
+            self.topImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.0),
+            self.topImageView.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.0),
+            self.open.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.0)
+        ])
+    }
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
